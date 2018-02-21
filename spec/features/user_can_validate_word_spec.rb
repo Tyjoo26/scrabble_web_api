f63a88b0ff36d45b0bc6d2b94921a252735f6fd9
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-21 09:17:43 -0700

    build out feature test for validation, building out form

---
+++ spec/features/user_can_validate_word_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "as a user when i visit / " do
+  context "i can fill in a text box with a foxes" do
+    it "allows me to validate word and will display a validation message" do
+
+
+      visit root_path
+
+      fill_in "q", with: "foxes"
+      click_button "Validate Word"
+
+      expect(current_path).to eq(root_path)
+
+      expect(page).to have_content("'#{word.valid}' is a valid word and its room form is '#{word.root}'")
+    end
+  end
+end
