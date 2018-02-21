07ac2f7c9a361f7dd6c3f90d30968b5c2267b890
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-21 11:27:18 -0700

    Figured it out...sorta

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,8 +1,8 @@
 class GameSerializer < ActiveModel::Serializer
   attributes :game_id
 
-  belongs_to :player_1, class_name: "User", foreign_key: :player_1_id
-  belongs_to :player_2, class_name: "User", foreign_key: :player_2_id
+  belongs_to :player_1, class_name: "User", foreign_key: :player_1_id, :key => :scores
+  belongs_to :player_2, class_name: "User", foreign_key: :player_2_id, :key => ""
 
 
 
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,12 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id , :score
+  attributes :user_id , :score
 
 
   def user_id
     object.id
   end
 
-  
+
   def score
     object.plays.sum(:score)
   end
