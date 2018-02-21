6b7b79723b9391f99114146dab22ff89870b9baa
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-21 12:02:11 -0700

    Changed variable name as the variable play has prebuilt meaning and fucntionality

--- app/controllers/api/v1/plays_controller.rb
+++ app/controllers/api/v1/plays_controller.rb
@@ -3,10 +3,11 @@ class Api::V1::PlaysController < ApplicationController
 
   def create
     game = Game.find(params[:game_id])
-    play = game.plays.new(play_params)
-    if play.save
-      render status: 200, json: play.to_json, :message => "201 Created Response"
+    plays = game.plays.new(play_params)
+    if plays.save
+      render status: 200, json: plays.to_json, :message => "201 Created Response"
     end
+    binding.pry
   end
 
 
