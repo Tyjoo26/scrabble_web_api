fe89ef9d1cb313a12ffe41e65a2cb629ce463445
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-27 16:32:30 -0700

    move logic from oxford service to input file

--- app/models/input.rb
+++ app/models/input.rb
@@ -7,17 +7,31 @@ class Input
     @root = attrs[:inflectionOf][0][:text]
   end
 
-  def self.map_input(word)
-    if call_oxford_service.map_json(word) == false
-      return false
+  def self.parse_json(word)
+    if OxfordService.new.get_json(word).status == 404
+      false
     else
-      call_oxford_service.map_json(word).first
+      JSON.parse(OxfordService.new.get_json(word).body, symbolize_names: true)[:results].first[:lexicalEntries]
     end
   end
 
-  private
-
-    def self.call_oxford_service
-      OxfordService.new
+  def self.map_json(word)
+    if parse_json(word) == false
+      return false
+    else
+      parse_json(word).map do |raw|
+        Input.new(raw)
+      end
     end
+  end
+
+  def self.map_input(word)
+    if map_json(word) == false
+      return false
+    else
+      map_json(word).first
+    end
+  end
+
+
 end
