02f5ef407476fae15d4a196a35728458ad588add
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-27 16:32:46 -0700

    Remove extra logic in oxford service so it only makes an api call

--- app/services/oxford_service.rb
+++ app/services/oxford_service.rb
@@ -1,20 +1,13 @@
 class OxfordService
 
-  attr_reader :conn
 
-
-  def map_json(word)
-    if parse_json(word) == false
-      return false
-    else
-      parse_json(word).map do |raw|
-        Input.new(raw)
-      end
-    end
+  def get_json(word)
+    @conn.get("inflections/en/#{word}")
   end
 
-
   private
+
+  attr_reader :conn
 
     def initialize
       @conn = Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1") do |faraday|
@@ -23,19 +16,5 @@ class OxfordService
         faraday.headers["app_key"]            = ENV["app_key"]
       end
     end
-
-    def get_json(word)
-      @conn.get("inflections/en/#{word}")
-    end
-
-    def parse_json(word)
-      if get_json(word).status == 404
-        false
-      else
-        JSON.parse(get_json(word).body, symbolize_names: true)[:results].first[:lexicalEntries]
-      end
-    end
-
-
 
 end
